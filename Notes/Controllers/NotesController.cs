a51e7ec7acd54472c17fd6cb932be298c80ed725
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-05-28 00:23:08 +0300

    Updates

---
+++ Notes/Controllers/NotesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Notes.Data;
+using Notes.Models;
+
+namespace Notes.Controllers
+{
+    public class NotesController : Controller
+    {
+        public IActionResult All()
+        {
+            var context = new NotesDbContext();
+            var notes = context.Notes.ToList();
+            var cats = new[] { "Пиле", "Муле" }.ToList();
+
+            Note note1 = new Note()
+            {
+                Id = 100,
+                Content = "Class created content",
+                CreationTime = DateTime.Now,
+                AuthorId = 11,
+                Author = new User()
+                {
+                    Id = 11,
+                    Username = "Страхил",
+
+                }
+            };
+
+            Note note2 = new Note()
+            {
+                Id = 101,
+                Content = "Class created content2",
+                CreationTime = DateTime.Now,
+                AuthorId = 12,
+                Author = new User()
+                {
+                    Id = 12,
+                    Username = "Панакуди",
+
+                }
+            };
+
+            List<Note> notes2 = new List<Note>();
+            notes2.Add(note1);
+            notes2.Add(note2);
+
+
+
+
+            return this.View(notes2);
+        }
+    }
+}
--- Notes/Models/Note.cs
+++ Notes/Models/Note.cs
@@ -4,7 +4,7 @@ namespace Notes.Models
 {
     public class Note
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
 
         public string Content { get; set; }
 
