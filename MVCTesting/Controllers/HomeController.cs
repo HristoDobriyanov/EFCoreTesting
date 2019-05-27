53574194ebe7930a80852d6cbaf6d60fc069fe2d
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-05-27 15:25:15 +0300

    Initial commit

---
+++ MVCTesting/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCTesting.Models;
+
+namespace MVCTesting.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Some()
+        {
+            string[] cats = new[] {"Пиле", "Миле", "Жоро", "Стамат" };
+
+
+            return View(model: cats);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PagesTesting/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PagesTesting.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string[] Cats;
+
+
+        public void OnGet()
+        {
+            Cats = new[] {"Боби", "Пешо", "Пильо"};
+        }
+    }
+}
