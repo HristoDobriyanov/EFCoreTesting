51939395d3569ac09395a8e5cc35b76fe82d1942
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-05-28 14:13:59 +0300

    Updates

--- Notes/Controllers/NotesController.cs
+++ Notes/Controllers/NotesController.cs
@@ -13,44 +13,8 @@ namespace Notes.Controllers
         {
             var context = new NotesDbContext();
             var notes = context.Notes.ToList();
-            var cats = new[] { "Пиле", "Муле" }.ToList();
 
-            Note note1 = new Note()
-            {
-                Id = 100,
-                Content = "Class created content",
-                CreationTime = DateTime.Now,
-                AuthorId = 11,
-                Author = new User()
-                {
-                    Id = 11,
-                    Username = "Страхил",
-
-                }
-            };
-
-            Note note2 = new Note()
-            {
-                Id = 101,
-                Content = "Class created content2",
-                CreationTime = DateTime.Now,
-                AuthorId = 12,
-                Author = new User()
-                {
-                    Id = 12,
-                    Username = "Панакуди",
-
-                }
-            };
-
-            List<Note> notes2 = new List<Note>();
-            notes2.Add(note1);
-            notes2.Add(note2);
-
-
-
-
-            return this.View(notes2);
+            return this.View(notes);
         }
     }
 }
