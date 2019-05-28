fde53b90d30fd2b0d224bae4651658afbcbf5471
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-05-28 16:00:25 +0300

    Updates

--- Notes/Controllers/NotesController.cs
+++ Notes/Controllers/NotesController.cs
@@ -9,12 +9,38 @@ namespace Notes.Controllers
 {
     public class NotesController : Controller
     {
+        public NotesController(NotesDbContext context)
+        {
+            this.DbContext = context;
+        }
+
+        public NotesDbContext DbContext { get; set; }
+
         public IActionResult All()
         {
-            var context = new NotesDbContext();
-            var notes = context.Notes.ToList();
+           
+            var notes = this.DbContext.Notes.ToList();
 
             return this.View(notes);
+        }
+
+        [HttpGet]
+        public IActionResult New() => this.View();
+
+        [HttpPost]
+        public IActionResult New(string content)
+        {
+            Note note = new Note()
+            {
+                AuthorId = 2,
+                Content = content,
+                CreationTime = DateTime.Now
+            };
+
+            this.DbContext.Notes.Add(note);
+            this.DbContext.SaveChanges();
+
+            return this.RedirectToAction("All", "Notes");
         }
     }
 }
---
+++ PagesTesting/Pages/Cats.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PagesTesting.Pages
+{
+    public class CatsModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Hello to cats page!";
+        }
+    }
+}
